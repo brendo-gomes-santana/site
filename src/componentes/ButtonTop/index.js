7d6052b374836f3bdef17a4ec89f48a9f3e99a74
Brendo Gomes Santana <brendogs2021@gmail.com>
2023-05-31 14:35:06 -0300

    atualizando site

--- src/componentes/ButtonTop/index.js
+++ src/componentes/ButtonTop/index.js
@@ -5,8 +5,8 @@ export default function ButtonTop() {
   return (
     <a href='#eu' id='buttonSubir' style={{
       position: 'fixed',
-      bottom: '2%',
-      right:'2%'
+      bottom: '3%',
+      right:'3%'
     }}>
         <BsFillArrowUpCircleFill style={{
           fontSize: '3rem',
